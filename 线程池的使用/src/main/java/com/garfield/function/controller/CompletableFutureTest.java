c491e88eb41c5977b48156eab48aa41ee29c0463
treezhu <zhu_shizhu@163.com>
2020-09-27 21:29:37 +0800

    合并请求

--- 线程池的使用/src/main/java/com/garfield/function/controller/CompletableFutureTest.java
+++ 线程池的使用/src/main/java/com/garfield/function/controller/CompletableFutureTest.java
@@ -1,11 +1,10 @@
 package com.garfield.function.controller;
 
-import oracle.jrockit.jfr.events.RequestableEventEnvironment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
-import org.springframework.data.redis.connection.RedisConnection;
-import org.springframework.data.redis.connection.StringRedisConnection;
-import org.springframework.data.redis.core.*;
+import org.springframework.data.redis.core.RedisOperations;
+import org.springframework.data.redis.core.SessionCallback;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,7 +12,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.PostConstruct;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.*;
 
 @Controller
@@ -26,7 +26,7 @@ public class CompletableFutureTest {
     @PostConstruct
     public void response(){
         ScheduledThreadPoolExecutor scheduledThreadPoolExecutor = new ScheduledThreadPoolExecutor(1);
-        scheduledThreadPoolExecutor.scheduleWithFixedDelay(new Runnable() {
+        scheduledThreadPoolExecutor.scheduleAtFixedRate(new Runnable() {
             @Override
             public void run() {
                 List<Request> requestList = new ArrayList();
@@ -34,32 +34,27 @@ public class CompletableFutureTest {
                     for (int i = 0; i < queue.size(); i++) {
                         requestList.add(queue.poll());
                     }
-                    Map<String,CompletableFuture<String>> movieIdMap = new HashMap<>();
                     List<String> keyList = new ArrayList<>();
                     requestList.forEach(x->{
                         String movieId = x.getMovieId();
-                        CompletableFuture<String> completableFuture = x.getCompletableFuture();
-                        movieIdMap.put(movieId,completableFuture);
                         keyList.add(movieId);
                     });
+                    System.out.println("批量查询"+keyList.size()+"条");
                     //从redis里批量查询
                     List<Object> list = stringRedisTemplate.executePipelined(new SessionCallback<String>() {
                         @Override
-                        public <K, V> String execute(RedisOperations<K, V> redisOperations) throws DataAccessException {
-                            keyList.forEach(k->{
-                                stringRedisTemplate.opsForValue().get(k);
-                            });
+                        public String execute(RedisOperations redisOperations) throws DataAccessException {
+                            keyList.forEach(k-> redisOperations.opsForValue().get(k));
                             return null;
                         }
                     });
                     if(CollectionUtils.isEmpty(list)){
                         return;
                     }
-                    for (int i = 0; i < list.size(); i++) {
-                        String s = (String) list.get(i);
-                        CompletableFuture<String> mapCompletableFuture = movieIdMap.get(keyList.get(i));
-                        mapCompletableFuture.complete(s);
-
+                    for (int i = 0; i < requestList.size(); i++) {
+                        Request request = requestList.get(i);
+                        String result = (String) list.get(i);
+                        request.getCompletableFuture().complete(result);
                     }
                 }
             }
