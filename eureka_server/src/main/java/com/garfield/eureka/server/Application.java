d2f7414a254a5c401367419f6f530acf26864d83
“tes”jingliyuangit config --global user.email “tjingliyuan@ewell.cc <jingliyuan@ewell.cc>
2020-12-25 13:05:52 +0800

    eureka

---
+++ eureka_server/src/main/java/com/garfield/eureka/server/Application.java
@@ -0,0 +1,17 @@
+package com.garfield.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author jingliyuan
+ * @date 2020/12/25
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
