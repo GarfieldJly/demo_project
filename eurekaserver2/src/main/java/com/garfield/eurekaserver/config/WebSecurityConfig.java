e40a384a15a60ecf86eb87d91928cab1efacedce
“tes”jingliyuangit config --global user.email “tjingliyuan@ewell.cc <jingliyuan@ewell.cc>
2021-01-02 20:55:45 +0800

    hystrix

--- eurekaserver2/src/main/java/com/garfield/eurekaserver/config/WebSecurityConfig.java
+++ eurekaserver2/src/main/java/com/garfield/eurekaserver/config/WebSecurityConfig.java
@@ -1,19 +1,19 @@
-package com.garfield.eurekaserver.config;
-
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-/**
- * @author jingliyuan
- * @date 2020/12/28
- */
-@EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().ignoringAntMatchers("/eureka/**");
-        super.configure(http);
-    }
-}
+//package com.garfield.eurekaserver.config;
+//
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+///**
+// * @author jingliyuan
+// * @date 2020/12/28
+// */
+//@EnableWebSecurity
+//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.csrf().ignoringAntMatchers("/eureka/**");
+//        super.configure(http);
+//    }
+//}
